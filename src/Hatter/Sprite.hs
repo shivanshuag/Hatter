a5811c11e50033ab5e60bd7935a10ab2c6c0af6c
Ayush Mittal <ayushsmittal@gmail.com>
2015-11-09 23:39:29 +0530

    Implemented loadTexture function for loading image to texture

--- src/Hatter/Sprite.hs
+++ src/Hatter/Sprite.hs
@@ -1,3 +1,15 @@
 module Hatter.Sprite where
 
+import qualified Graphics.UI.SDL as SDL
+import qualified Graphics.UI.SDL.Image as Image
+import Foreign.C.String
+
 data Sprite = Sprite{file :: String, width :: Int, height :: Int}
+
+loadTexture :: SDL.Renderer -> String -> IO SDL.Texture
+loadTexture renderer path = Image.imgLoadTexture renderer path >>=either throwSDLError return
+
+throwSDLError :: String -> IO a
+throwSDLError message = do
+        errorString <- SDL.getError >>= peekCString
+        fail (message ++ "SDL_Error:" ++ errorString)
