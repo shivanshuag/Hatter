f5ccd3e8d46dae388b8db25141733fa290ec278b
Ayush Mittal <ayushsmittal@gmail.com>
2015-11-10 01:15:00 +0530

    Updated loadTexture and AssetStore

--- src/Hatter/AssetStore.hs
+++ src/Hatter/AssetStore.hs
@@ -2,7 +2,13 @@ module Hatter.AssetStore where
 
 import Data.Map
 import qualified SDL
+import Data.Lens.Common
 
-data AssetStore = AssetStore {images :: Map String SDL.Texture
+data AssetStore = AssetStore {images :: Map String (IO SDL.Texture)
 --                             , sounds ::
                              }
+
+updateImageStore :: AssetStore -> path -> IO SDL.Texture -> AssetStore
+updateImageStore oldstore path texture = 
+        let newImageStore = Map.insert path texture $ images oldstore
+            in setL images newImageStore oldstore 
--- src/Hatter/Sprite.hs
+++ src/Hatter/Sprite.hs
@@ -1,13 +1,23 @@
 module Hatter.Sprite where
 
+import Hatter.AssetStore
 import qualified Graphics.UI.SDL as SDL
 import qualified Graphics.UI.SDL.Image as Image
 import Foreign.C.String
+import qualified Data.Map as Map
+import Control.Monad.IO.Class (liftIO)
 
 data Sprite = Sprite{file :: String, width :: Int, height :: Int}
 
-loadTexture :: SDL.Renderer -> String -> IO SDL.Texture
-loadTexture renderer path = Image.imgLoadTexture renderer path >>=either throwSDLError return
+loadTexture :: SDL.Renderer -> String -> AssetStore ->(IO SDL.Texture, AssetStore)
+loadTexture renderer path assetStore = case lookup path $ images assetStore of
+    Just texture -> (texture, assetStore)
+    Nothing -> let texture = loadImageFromFile renderer path 
+                   in (texture, updateImageStore assetStore path texture)
+
+loadImageFromFile :: SDL.Renderer -> String -> IO SDL.Texture
+loadImageFromFile renderer path = Image.imgLoadTexture renderer path >>=either throwSDLError return
+                                        
 
 throwSDLError :: String -> IO a
 throwSDLError message = do
