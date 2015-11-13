5b898c46bb5a2000f27f5e5484968f7deafd1f42
Shivanshu Agrawal <shivanshuag@gmail.com>
2015-11-13 12:44:15 +0530

    changed parameters in 2body

--- examples/2body.hs
+++ examples/2body.hs
@@ -7,28 +7,28 @@ import Control.Wire as Wire hiding (id)
 import Linear
 
 g :: Double
-g = 6.6e-13
+g = 1
 
 assetdir :: String
 assetdir = "examples/assets/"
 data Mass = Mass{mass1 :: Double
                 ,mass2 :: Double}
 
-mass = Mass{mass1=12e+19
-           ,mass2=20e+1}
+mass = Mass{mass1=2500000
+           ,mass2=2500000}
 
 body1 :: GameObject
 body1 = GameObject{oid="body1"
                   ,position=V2 10 300
-                  ,boundingBox=Circle (V2 60 60) 60
-                  ,gameGraphic=Image Sprite{file=assetdir++"planet1.png",width=120,height=120}
+                  ,boundingBox=Circle (V2 24 24) 24
+                  ,gameGraphic=Image Sprite{file=assetdir++"planet1.png",width=48,height=48}
                   }
 
 body2 :: GameObject
 body2 = GameObject{oid="body2"
                   ,position=V2 500 300
-                  ,boundingBox=Circle (V2 50 50) 50
-                  ,gameGraphic=Image Sprite{file=assetdir++"planet2.png",width=100,height=100}
+                  ,boundingBox=Circle (V2 10 10) 20
+                  ,gameGraphic=Image Sprite{file=assetdir++"planet2.png",width=40,height=40}
                   }
 
 acceleration1Wire :: Wire s e IO (GameState Mass) (V2 Double)
@@ -53,13 +53,13 @@ posdiff state =
   let objects = gameObjects state
       (object1, object2) = (Map.lookup "body1" objects, Map.lookup "body2" objects)
   in case (object1, object2) of
-    (Just o1, Just o2) -> (position o1 + V2 60 60)  - (position o2 + V2 50 50)
+    (Just o1, Just o2) -> (position o1 + V2 12 12)  - (position o2 + V2 10 10)
     (_,_) -> V2 0 0
 
 body1Wire :: (Real t, HasTime t s) => Wire s e IO (GameState Mass) GameObject
 body1Wire = proc state -> do
   let object = extractObject $ Map.lookup "body1" $ gameObjects state
-  newposition <- positionWire identityCorrectionFunction (V2 400 400) (velocityWire identityCorrectionFunction $ V2 0 2) acceleration1Wire -< state
+  newposition <- positionWire identityCorrectionFunction (V2 400 400) (velocityWire identityCorrectionFunction $ V2 0 50) acceleration1Wire -< state
   let newobject = GameObject {oid=oid object, position=newposition, boundingBox=boundingBox object, gameGraphic=gameGraphic object}
   returnA -< newobject
 
@@ -70,7 +70,7 @@ extractObject Nothing = body1
 body2Wire :: (Real t, HasTime t s) => Wire s e IO (GameState Mass) GameObject
 body2Wire = proc state -> do
   let object = extractObject $ Map.lookup "body2" $ gameObjects state
-  newposition <- positionWire identityCorrectionFunction (V2 900 400) (velocityWire identityCorrectionFunction $ V2 0 $ negate 120) acceleration2Wire -< state
+  newposition <- positionWire identityCorrectionFunction (V2 600 400) (velocityWire identityCorrectionFunction $ V2 0 $ negate 50) acceleration2Wire -< state
   let newobject = GameObject {oid=oid object, position=newposition, boundingBox=boundingBox object, gameGraphic=gameGraphic object}
   returnA -< newobject
 
